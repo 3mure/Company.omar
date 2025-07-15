e7c4d7823146531d52dce0354d6fdc9f91386035
Administrator <Administrator@DESKTOP-854KJI4>
2025-07-15 17:06:08 +0300

    Department module PLL

---
+++ Company.omar.PLL/Interface/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.omar.DAL.Model;
+
+namespace Company.omar.PLL.Interface
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+        Department GetDepartment(int id);
+        int AddDepartment(Department Modle);
+        int UpdateDepartment(Department Modle);
+        int DeleteDepartment(Department Modle);
+
+    }
+}
---
+++ Company.omar.PLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.omar.DAL.Data.context;
+using Company.omar.DAL.Model;
+using Company.omar.PLL.Interface;
+
+namespace Company.omar.PLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly APPDbContext _context;
+
+        public DepartmentRepository()
+        {
+         _context = new APPDbContext();
+        }
+
+        public Department GetDepartment(int id)
+        {
+            return _context.Departments.Find(id); 
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _context.Departments.ToList();
+        }
+        public int AddDepartment(Department Modle)
+        {
+            _context.Departments.Add(Modle);
+           return _context.SaveChanges();
+
+        }
+        
+        public int UpdateDepartment(Department Modle)
+        {
+            _context.Departments.Update(Modle);
+            return _context.SaveChanges();
+        }
+
+        public int DeleteDepartment(Department Modle)
+        {
+            _context.Departments.Remove(Modle);
+            return _context.SaveChanges();
+        }
+
+       
+
+        
+
+        
+    }
+}
