9fdc0c22a3b0718f6bf39fe8f541d031db9d8464
Administrator <Administrator@DESKTOP-854KJI4>
2025-07-15 16:16:55 +0300

    Add project files.

---
+++ Company.omar.DAL/Data/Configrations/DepartmentConfigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.omar.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.omar.DAL.Data.Configrations
+{
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Department> builder)
+        {
+           builder.HasKey(d => d.Id);
+            builder.Property(d => d.Id).HasColumnName("DepartmentId").UseIdentityColumn(10,10);
+
+            builder.Property(D=> D.code)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(d => d.Name)
+                   .IsRequired()
+                   .HasMaxLength(100)
+                   .HasColumnName("DepartmentName");
+            builder.Property(d => d.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("getdate()");
+
+
+
+        }
+    }
+}
---
+++ Company.omar.DAL/Data/context/APPDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Company.omar.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.omar.DAL.Data.context
+{
+    public class APPDbContext : DbContext
+    {
+        public APPDbContext() : base()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=.;Database=Company.omar; Trusted_Connection=True;TrustServerCertificate = True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department>Departments { get; set; }
+
+    }
+}
